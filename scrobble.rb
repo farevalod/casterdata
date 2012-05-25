299f91ab8eee9ef53e0a26e7d2c03879b8b9474e
Francisco Arevalo <farevalod@gmail.com>
2012-05-24 22:18:56 -0400

    Handled "radio down" case
    FA

--- scrobble.rb
+++ scrobble.rb
@@ -8,9 +8,12 @@ uri = URI("http://184.173.181.32:19025/status.xsl")
 listeners = $1
 /<Current-Song>(.*)<\/Current-Song>/.match(@txt)
 song = $1
+if(song)
 cgi.out{
   cgi.html{
 	cgi.head{'<meta http-equiv="refresh" content="10; URL=scrobble.rb">'} +
     cgi.body{"<div style='overflow:hidden;background:#141414;color:#f0f7ff;font-weight:bold;vertical-align:middle;margin-top:28px;'><span style='font-size:22px'>Now Playing: <span style='color:#bbbbff'>"+song+"</span></span><br/><span style='font-size:18px'>Current Listeners: <span style='color:#bbbbff'>"+listeners+"</span></span></div>"}
   }
 }
+else
+	cgi.out{cgi.html{cgi.body{"Radio is offline"}}}
