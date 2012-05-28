29dfdff00a194ce69a14438402a5992ce481d8a1
Francisco Arevalo <farevalod@gmail.com>
2012-05-28 01:14:28 -0400

    Added "song_history" feature
    FA

--- scrobble.rb
+++ scrobble.rb
@@ -12,8 +12,15 @@ if(song)
 cgi.out{
   cgi.html{
 	cgi.head{'<meta http-equiv="refresh" content="10; URL=scrobble.rb">'} +
-    cgi.body{"<div style='overflow:hidden;background:#141414;color:#f0f7ff;font-weight:bold;vertical-align:middle;margin-top:28px;'><span style='font-size:22px'>Now Playing: <span style='color:#bbbbff'>"+song+"</span></span><br/><span style='font-size:18px'>Current Listeners: <span style='color:#bbbbff'>"+listeners+"</span></span></div>"}
+    cgi.body{"<div style='overflow:hidden;background:#141414;color:#f0f7ff;font-weight:bold;vertical-align:middle;margin-top:28px;'><span style='font-size:22px'>Now Playing: <span style='color:#bbbbff'>"+song+"</span></span><br/><span style='font-size:18px'>Current Listeners: <span style='color:#bbbbff'>"+listeners+"</span></span><br/><a style='color:#bbbbff' href='http://4ws.cl/song_history.txt' target='new'>Last Played</a></div>"}
   }
+}
+last_line = `tail -n 1 song_history.txt`
+time = Time.new
+File.open("song_history.txt", 'a'){|f| 
+	if(last_line.split(': ')[1].chomp != song)
+		f.write(time.inspect+": "+song+"\n")
+	end
 }
 else
 	cgi.out{cgi.html{cgi.head + cgi.body{"Radio is offline"}}}
